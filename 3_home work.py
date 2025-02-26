a4e3b365055581ea27d10fe38a27ef9be8ae48c8
NazarZhulevych <nazarzhulevych@gmail.com>
2025-02-26 23:57:28 +0100

    Task 2

--- 3_home work.py
+++ 3_home work.py
@@ -2,15 +2,22 @@ from datetime import datetime
 
 
 def string_to_date(date_string):
-    date_object = datetime.strptime(date_string, "%Y.%m.%d").date()
-    return date_object
+    return datetime.strptime(date_string, "%Y.%m.%d").date()
 
-def date_to_string(date):
-    date_object = datetime.strftime(date, "%Y.%m.%d")
-    return date_object
 
-date_string = "2024.01.01"
-converted_date = string_to_date(date_string)
-print(converted_date)
-date_string = date_to_string(converted_date)
-print(date_string)
+def prepare_user_list(user_data):
+    for user in user_data:  # Iterate through the list of dictionaries
+        if "birthday" in user:  # Ensure the key exists
+            user["birthday"] = string_to_date(user["birthday"])  # Update the dictionary
+    return user_data
+
+users = [
+    {"name": "Bill Gates", "birthday": "1955.3.25"},
+    {"name": "Steve Jobs", "birthday": "1955.3.21"},
+    {"name": "Jinny Lee", "birthday": "1956.3.22"},
+    {"name": "John Doe", "birthday": "1985.01.23"},
+    {"name": "Jane Smith", "birthday": "1990.01.27"}
+]
+
+prepared_users = prepare_user_list(users)
+print(prepared_users)
