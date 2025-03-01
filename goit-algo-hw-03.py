dd4adab09905f44a43351a6007186013d3ae59bd
NazarZhulevych <nazarzhulevych@gmail.com>
2025-03-01 15:05:36 +0100

    exept add

--- goit-algo-hw-03.py
+++ goit-algo-hw-03.py
@@ -1,12 +1,16 @@
 from datetime import datetime
 
-def get_days_from_today(date) ->int:
-    current_date = datetime.now().date()
-    date = datetime.strptime(date, "%Y-%m-%d").date()
-    days_from_date = (current_date - date).days
+def get_days_from_today(date: str) ->int:
+    try:
+        current_date = datetime.now().date()
+        date = datetime.strptime(date, "%Y-%m-%d").date()
+        days_from_date = (current_date - date).days
+        return days_from_date
+    except ValueError:
+        print("Помилка: Неправильний формат дати! Використовуйте YYYY-MM-DD.")
+        return None
     
-    return days_from_date
 
-input_date = "2020-10-09"
+input_date = input("Введіть дату у форматі YYYY-MM-DD, (наприклад: 2012-12-12) :")
 
 print(get_days_from_today(input_date))
